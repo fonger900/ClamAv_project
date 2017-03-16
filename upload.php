56de01cd216a687c963e1f41241701972c1a2a5b
Phong <fonger900@gmail.com>
2017-03-16 23:31:41 +0700

    Modified

--- upload.php
+++ upload.php
@@ -4,16 +4,16 @@ $target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $out = "";
 $command = "clamdscan ".$target_file;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+//$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 //check if file already exists
-if(file_exists($target_file))
+/*if(file_exists($target_file))
 {
-	echo "Sorry, file already exists";
+	echo "Sorry, file already exists"."<br>";
 	$uploadOk = 0;
-}
+}*/
 //check file size
-if($_FILES["fileToUpload"]["size"]>50000000){
-	echo "Sorry, your file is too large";
+if($_FILES["fileToUpload"]["size"]>1024*1024){
+	echo "Sorry, your file is too large"."<br>";
 	$uploadOk = 0;
 }
 //check if $uploadOk is set to 0 by an error
@@ -23,7 +23,7 @@ if($uploadOk == 0){
 	if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$target_file)){
 		echo "The file ".basename($target_file)." has been uploaded";
 		//check for virus
-		
+
 		echo "<br>"."Scan result: ";
 		echo passthru($command);
 		exec("rm ".$target_file);
