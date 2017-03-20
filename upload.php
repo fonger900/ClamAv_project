52df7603df83a2667010be8ac78a8fb08995ab26
ubuntu <root@haianh.com>
2017-03-20 16:47:08 +0700

    Modified

--- upload.php
+++ upload.php
@@ -1,65 +1,87 @@
+
 <?php
 $target_dir = "/home/ngo/store_file/";
 $target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
-$file = basename($target_file);
-$uploadOk = 1;
-$out = "";
-$command = "clamdscan ".$target_file;
+$command = "echo SCAN ".$target_file." | nc -U /var/run/clamav/clamd.ctl";
 $fileTmpLoc=$_FILES["fileToUpload"]["tmp_name"];
-//$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-//check if file already exists
-/*if(file_exists($target_file))
-{
-	echo "Sorry, file already exists"."<br>";
-	$uploadOk = 0;
-}*/
-//echo "File size: ".filesize($target_dir.$file)."<br>";
-
-//check file size
-//if(isset($_FILES['fileToUpload']))
-//{
+$file = basename($target_file);
+$flength = strlen($file);
+$out = "";
+$virus ="";
+$virus_detected = 0;
+$is_uploaded = 0;
+$is_selected = 0;
+$uploadOk = 1;
 //check if file is selected
 if(!$fileTmpLoc){
-	 echo "Error! File is not selected or too large";
+	 //echo "Error! File is not selected or too large";
+	 $is_selected = 0;
 	//$uploadOk=0;
 }else{
-//check file size
-/*if($_FILES["fileToUpload"]["size"]>1048576){
-	echo "Sorry, your file is too large"."<br>";
-	$uploadOk = 0;
-}*/
-//check if $uploadOk is set to 0 by an error
-if($uploadOk == 0){
-	echo "Your file was not uploaded";
-	
-}else{
+	$is_selected = 1;
+	//if file uploaded successfully
 	if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$target_file)){
-		echo "The file ".basename($target_file)." has been uploaded";
+		$is_uploaded = 1;
 		//check for virus
-
-		echo "<br>"."Scan result: ";
 		exec($command,$out);
-		//print_r($out);
 		$FOUNDpos = strpos($out[0],"FOUND");
-		//echo "Virus: ".$FOUNDpos;
-		if( $FOUNDpos !=FALSE){
-			echo "Virus FOUND"."<br>";
-			$flength = strlen($file);
+		if( $FOUNDpos != FALSE){
+			$virus_detected = 1;
 			$name_pos = strpos($out[0],$file);//
-			echo "====================================="."<br>";
-			echo "File name: ".$file." || ";
 			//get virus name
 			$virus = substr($out[0],$name_pos+$flength,$FOUNDpos);
 			$virus = substr($virus,1,strlen($virus)-6);
-			echo "Virus: ".$virus;
 		}
-		else echo "No virus detected";
+		//remmove uploaded file
 		exec("rm ".$target_file);
-
-	}else{ 
-		echo "Sorry, there was an error uploading your file";
 	}
 }
-}
-//}else echo "No file choosed or your file is too large!";
 ?>
+<!DOCTYPE html>
+<head>
+	<title>
+		Result
+	</title>
+	<style type="text/css">
+		table, th, td {
+			border:1px solid black;
+			border-collapse: collapse;
+		}
+	</style>
+</head>
+<body>
+<?php if($is_selected==0){ ?>
+	<span>Error! File is not selected</span>
+	<?php }else{ ?>
+	<?php if($is_uploaded==0){ ?>
+		 <span>Error! File is not uploaded</span>
+		 <?php }else{ ?>
+		 	<?php if($virus_detected==0){ ?>
+		 		<p>No virus found</p><br>
+		 		<table style="width: 100%">
+					<tr>
+						<td>
+						File: <?php echo $file; ?>
+						</td>
+						<td>
+						Virus: <?php echo $virus; ?>
+						</td>
+					</tr>
+				</table>
+		 		<?php }else{ ?>
+				<p>Virus found</p><br>
+				<table style="width: 100%">
+					<tr>
+						<td>
+						File: <?php echo $file; ?>
+						</td>
+						<td>
+						Virus: <?php echo $virus; ?>
+						</td>
+					</tr>
+				</table>
+				<?php } ?>
+			<?php } ?>
+		<?php } ?>
+</body>
+</html>
